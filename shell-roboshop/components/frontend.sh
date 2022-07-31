45978db21576df60ff79bc985d3a3ee822e38142
amohanty <amohantyx@gmail.com>
2022-07-31 17:35:27 +0530

    roboshop shell

--- shell-roboshop/components/frontend.sh
+++ shell-roboshop/components/frontend.sh
@@ -1,4 +1,7 @@
 echo 'installing frontend'
+yum install nginx -y
+systemctl enable nginx
+systemctl start nginx
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 cd /usr/share/nginx/html
 rm -rf *
